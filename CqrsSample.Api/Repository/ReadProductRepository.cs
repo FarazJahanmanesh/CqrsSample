a75c4a6dc7a11e566e49e2c84ff4e8118db16ba6
farazasax@outlook.com <farazasax@outlook.com>
2024-07-31 16:22:57 +0330

    add dbcontext to repo

--- CqrsSample.Api/Repository/ReadProductRepository.cs
+++ CqrsSample.Api/Repository/ReadProductRepository.cs
@@ -4,4 +4,9 @@ namespace CqrsSample.Api.Repository;
 
 public class ReadProductRepository: IReadProductRepository
 {
+    private readonly ProdcutDbContext _context;
+    public ReadProductRepository(ProdcutDbContext context)
+    {
+        _context = context;
+    }
 }
