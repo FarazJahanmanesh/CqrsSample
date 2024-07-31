e648e2ddce2731d3830ac3bff848b951d791b299
farazasax@outlook.com <farazasax@outlook.com>
2024-07-31 16:30:25 +0330

    appsetting and adding the db and ioc

--- CqrsSample.Api/ReadUnitOfWork.cs
+++ CqrsSample.Api/ReadUnitOfWork.cs
@@ -5,12 +5,18 @@ namespace CqrsSample.Api;
 
 public class ReadUnitOfWork: IReadUnitOfWork
 {
+    private readonly ProdcutDbContext dbContext;
     private ReadProductRepository _readProductRepository;
+
+    public ReadUnitOfWork(ProdcutDbContext _dbContext)
+    {
+        dbContext = _dbContext;
+    }
     public IReadProductRepository ReadProductRepository
     {
         get
         {
-            return _readProductRepository ??= new ReadProductRepository();
+            return _readProductRepository ??= new ReadProductRepository(dbContext);
         }
     }
 }
