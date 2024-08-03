c1573b48fa1ef99e5804ef98a2b90737bd659354
farazasax@outlook.com <farazasax@outlook.com>
2024-08-03 16:38:39 +0330

    add some bases

---
+++ Cqrs.Api/Controllers/BaseController.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cqrs.Api.Controllers;
+
+[Route("api/[controller]/[action]")]
+[ApiController]
+public class BaseController : ControllerBase
+{
+    private readonly ISender _sender;
+    public BaseController(ISender sender)
+    {
+        _sender = sender;
+    }
+}
